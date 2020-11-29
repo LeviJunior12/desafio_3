26ca817e822c2571fc1555e02fb708511d98ff6d
LeviJunior12 <levicoc123@gmail.com>
2020-11-29 19:25:54 -0300

    Tela Splash Screen

---
+++ app/src/main/java/com/example/desafio_3/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.desafio_3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/desafio_3/ui/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.desafio_3.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.desafio_3.MainActivity
+import com.example.desafio_3.R
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        ivSplash.alpha = 0f
+        ivSplash.animate().setDuration(1500).alpha(1f).withEndAction {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+    }
+}
